28d8415cf76c76388deff72db42c42ef855d5402
soypat <graded.sp@gmail.com>
2018-09-16 02:31:26 -0300

    Merge finalize

--- fea/ejemplo/Clase 13/Notas de clase.m
+++
@@ -1,3 +0,0 @@
-% Hay que disminuir 1 grado de integración por Gauss para conseguir la
-% superconvergencia, es decir sub integrar.
-% Para un Q4 hay que integrar con un sólo punto en el medio.
--- fea/ejemplo/clase 1/por el colo/Ej2Practica1/linearLoad.m
+++
@@ -1,7 +0,0 @@
-% Toma un t?rmino independiente y una pendiente y saca una funci?n lineal
-% dependiente de x con esos par?metros
-
-function[P] = linearLoad(yIntercept,gradient)
-
-P = @(x) yIntercept+gradient*x;
-end
