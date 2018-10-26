908e37c78b41b849aac4865ca6b9458d7125442d
acanalis <acanalis@itba.edu.ar>
2018-10-26 01:03:51 -0300

    Corrected TP1

--- TP1 Transferencia/TP1.m
+++ TP1 Transferencia/TP1.m
@@ -12,35 +12,37 @@ T0=0;
 Tamb=30;
 hamb=10; %W/m^2K
 %% Begin problem
-Ne=10; %Elementos
-N=Ne+1;
-Le=L/Ne; %long. elementos
-nodos=0:Le:L
-C=zeros(N);
-Q=zeros(N,1);
+Nx=6; %cantidad de elementos (2 medios volumenes y Nx-2 completos )
+dx=L/(Nx-1); %long. elementos
+nodos=0:dx:L;
+C=zeros(Nx);
+Q=zeros(Nx,1);
 
 %volumen 1
-C(1,1)=-3/2*k*Area/Le-hamb*pi*D*Le;
-C(1,2)=k*Area/Le;
-%Revisar
-
+C(1,1)=1;
+Q(1)=T0;
 %volumen N 
-C(end,end-1)=k*Area/Le;
-C(end,end)=-k*Area/Le-hamb*pi*D*Le; %Caso flujo fijado.
+C(end,end-1)=1/2*k*Area/dx;
+C(end,end)=1/2*(-k*Area/dx-hamb*pi*D*dx); %Caso flujo fijado.
+Q(end)=1/2*(-qvol*Area*dx-hamb*pi*D*dx*Tamb);
 
-for i=2:Ne-1
+for i=2:Nx-1
     % No consideramos calor almacenado (inercia termica) d/dt=0 R.E.
-    C(i,i-1)=k*Area/Le;
-    C(i,i)=-2*k*Area/Le-hamb*pi*D*Le;
-    C(i,i+1)=k*Area/Le;
+    C(i,i-1)=k*Area/dx;
+    C(i,i)=-2*k*Area/dx-hamb*pi*D*dx;
+    C(i,i+1)=k*Area/dx;
+    Q(i)=-qvol*Area*dx-hamb*pi*D*dx*Tamb;
 end
 
-Q(2:end)=-qvol*Area*Le-hamb*pi*D*Le*Tamb;
-
 T=C\Q;
-midpoint=@(X) (X(1:end-1)+X(2:end))/2;
 figure(1)
-plot(nodos,T,'*',[0 nodos],solucion_analitica1([0,nodos]))
+hold on
+x_sol=0:0.05:1;
+plot(x_sol,solucion_analitica1(x_sol),'b')
+plot(nodos,T,'k-.*')
+legend('analitic','finite volume')
+hold off
+
 title('Perfil de Temperaturas')
 ylabel('Temperatura')
 xlabel('Posicion')
