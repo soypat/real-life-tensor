9febd1418f1330fb663d366414e45c43ecc8ad02
soypat <graded.sp@gmail.com>
2018-10-28 14:14:45 -0300

    Add error (MSE) graph

--- TP1 Transferencia/TP2.m
+++ TP1 Transferencia/TP2.m
@@ -2,9 +2,15 @@ k=1;
 L=.5;
 b=1;
 
-Nx=100; %Nodos en los que dividimos el problema
-Ny=100;
-
+Nx=4; %Nodos en los que dividimos el problema
+Ny=4;
+iterants=[fibonacci(7) fibonacci(8) fibonacci(9) fibonacci(10) fibonacci(11) fibonacci(12)];
+err=zeros(length(iterants),1);
+k=0;
+for N=iterants
+k=k+1;
+Nx=N;
+Ny=N;
 dx=L/(Nx-1);
 dy=b/(Ny-1);
 x=0:dx:L;
@@ -40,30 +46,34 @@ for j = 2:Nx-1
         C(DOF(i,j),DOF(i,j-1))=C(DOF(i,j),DOF(i,j-1))+1/dx^2;        
     end
 end
-
 T=C\Q;
 Tgrid=reshape(T,Ny,Nx);
 Texact=solucion_analitica2(x,y);
+err(k)=(numel(Tgrid)^-1)*sum(sum((Tgrid-Texact).^2));
+end
 
 % bar3(y,Tgrid)
-subplot(2,1,1)
+% subplot(3,1,1)
 % contourf(x,y,Tgrid)
 % title('Solución Obtenida')
 % subplot(2,1,2)
 % contourf(x,y,Texact)
 % title('Solución Exacta')
-subplot(1,2,1)
+subplot(1,3,1)
 surf1=surf(x,y,Tgrid,'edgecolor','none');
 title('Solución Obtenida')
  view(0,90)
  daspect([1 1 1])
-subplot(1,2,2)
+subplot(1,3,2)
 surf2=surf(x,y,Texact,'edgecolor','none');
  view(0,90)
  daspect([1 1 1])
 title('Solución Exacta')
-
-
+subplot(1,3,3);
+loglog(iterants.^2,err)
+title('Error cuadratico medio (Escala logarítmica)')
+xlabel('Cantidad de volúmenes');
+ylabel('Error')
 surf2.FaceColor='interp';
 %% Conservacion de energía
 % Qnwall=zeros(length(nwall),1)
